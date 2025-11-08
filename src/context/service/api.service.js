784fb3c698f4e0306bb2ee173cdece7496e422f1
bekicom <adelkauz6@gmail.com>
2025-11-08 15:11:19 +0500

    j

--- src/context/service/api.service.js
+++ src/context/service/api.service.js
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
+  // baseUrl: "http://localhost:5000",
 
-  // baseUrl: "https://sklad.richman.uz",
+  baseUrl: "https://sklad.richman.uz",
 
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("token");
