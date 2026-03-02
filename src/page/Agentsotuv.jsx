ef5faeb61cbeb515c37d65803010dc03101f5e69
bekicom <adelkauz6@gmail.com>
2026-03-02 23:54:08 +0500

    f

--- src/page/Agentsotuv.jsx
+++ src/page/Agentsotuv.jsx
@@ -628,7 +628,7 @@ export default function Agentsotuv() {
         }
 
         @media (max-width: 360px) {
-          .products-grid { grid-template-columns: repeat(1, minmax(0, 1fr)) !important; }
+          .products-grid { grid-template-columns: repeat(2, minmax(0, 1fr)) !important; }
         }
       `,
         }}
